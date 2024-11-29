888f7d97b4ee0e3e659880502dc87b15aa6428d4
Dillon Barnes <amber@transcental.dev>
2024-11-29 13:30:50 +0000

    add folder input component

---
+++ src/components/folderInput.svelte
@@ -0,0 +1,37 @@
+<script lang="ts">
+  import { open } from "@tauri-apps/plugin-dialog";
+
+  const openFolder = async () => {
+    const result = await open({
+      directory: true,
+      multiple: multiple,
+    });
+    if (result) {
+      callback(result);
+    }
+  };
+  export let multiple: boolean = false;
+  export let callback: (path: string) => void;
+</script>
+
+<button on:click={openFolder}>
+  <slot></slot>
+</button>
+
+<style>
+  button {
+    -webkit-appearance: none;
+    -moz-appearance: none;
+    appearance: none;
+    padding: 5px;
+    margin: 5px;
+    border: 1px solid #646cff;
+    border-radius: 5px;
+    background-color: #646cff;
+    color: white;
+  }
+
+  button {
+    cursor: pointer;
+  }
+</style>
