219b2cdeab5d6eab80cd513565e26f86ae9f863b
Amber <amber@transcental.dev>
2024-12-13 15:25:39 +0000

    add animation creation

---
+++ src/components/textInput.svelte
@@ -0,0 +1,33 @@
+<script lang="ts">
+  let {
+    disabled = false,
+    type,
+    value = $bindable(),
+  }: {
+    disabled?: boolean;
+    type: string;
+    value: string | number;
+  } = $props();
+</script>
+
+<input {type} {disabled} bind:value />
+
+<style>
+  input {
+    -webkit-appearance: none;
+    -moz-appearance: none;
+    appearance: none;
+    padding: 5px;
+    margin: 5px;
+    border: 1px solid #646cff;
+    border-radius: 5px;
+    background-color: #646cff;
+    color: white;
+  }
+
+  input[disabled] {
+    background-color: #646cff40;
+    color: #646cff80;
+    cursor: not-allowed;
+  }
+</style>
